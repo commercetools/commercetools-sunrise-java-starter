822becc5c9f4764745209571f5b07e9c0a268ad4
schleichardt <michael.schleichardt@commercetools.de>
2016-06-10 15:39:37 +0200

    inherit index action

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,20 +1,9 @@
 package controllers;
 
-import com.commercetools.sunrise.common.contexts.ProjectContext;
 import com.commercetools.sunrise.common.inject.RequestScoped;
-import com.commercetools.sunrise.productcatalog.home.HomeProductSuggestionsControllerComponent;
 import com.commercetools.sunrise.productcatalog.home.SunriseHomePageController;
-import play.mvc.Result;
-
-import javax.inject.Inject;
 
 @RequestScoped
 public class HomeController extends SunriseHomePageController {
-    @Inject
-    private ProjectContext projectContext;
-
-    public Result index() {
-        return TODO;
-    }
 
 }
