7f2fbf1b448acba8eea66b34d7bbc74292589a58
Laura Luiz <laura.luiz@commercetools.de>
2016-08-24 16:01:34 +0200

    Update project to sunrise framework 0.11.0 WIP

--- app/Module.java
+++ app/Module.java
@@ -1,7 +1,13 @@
-import com.commercetools.sunrise.common.controllers.ReverseRouter;
+import com.commercetools.sunrise.cms.CmsService;
+import com.commercetools.sunrise.common.httpauth.HttpAuthentication;
+import com.commercetools.sunrise.common.httpauth.basic.BasicAuthenticationProvider;
 import com.commercetools.sunrise.common.localization.LocationSelectorControllerComponent;
 import com.commercetools.sunrise.common.pages.DefaultPageNavMenuControllerComponent;
-import com.commercetools.sunrise.common.reverserouter.*;
+import com.commercetools.sunrise.common.template.cms.FileBasedCmsServiceProvider;
+import com.commercetools.sunrise.common.template.engine.HandlebarsTemplateEngineProvider;
+import com.commercetools.sunrise.common.template.engine.TemplateEngine;
+import com.commercetools.sunrise.common.template.i18n.ConfigurableI18nResolverProvider;
+import com.commercetools.sunrise.common.template.i18n.I18nResolver;
 import com.commercetools.sunrise.framework.MultiControllerComponentResolver;
 import com.commercetools.sunrise.framework.MultiControllerComponentResolverBuilder;
 import com.commercetools.sunrise.shoppingcart.MiniCartControllerComponent;
@@ -9,12 +15,8 @@ import com.commercetools.sunrise.shoppingcart.common.CheckoutCommonComponent;
 import com.commercetools.sunrise.shoppingcart.common.CheckoutStepWidgetComponent;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
-import io.sphere.sdk.utils.MoneyImpl;
-import routing.ReverseRouterImpl;
 
 import javax.inject.Singleton;
-import javax.money.Monetary;
-import javax.money.format.MonetaryFormats;
 
 /**
  * This class is a Guice module that tells Guice how to bind several
@@ -30,21 +32,14 @@ public class Module extends AbstractModule {
 
     @Override
     protected void configure() {
-        applyJavaMoneyHack();
-    }
-
-    private void applyJavaMoneyHack() {
-        //fixes https://github.com/commercetools/commercetools-sunrise-java/issues/404
-        //exception play.api.http.HttpErrorHandlerExceptions$$anon$1: Execution exception[[CompletionException: java.lang.IllegalArgumentException: java.util.concurrent.CompletionException: io.sphere.sdk.json.JsonException: detailMessage: com.fasterxml.jackson.databind.JsonMappingException: Operator failed: javax.money.DefaultMonetaryRoundingsSingletonSpi$DefaultCurrencyRounding@1655879e (through reference chain: io.sphere.sdk.payments.PaymentDraftImpl["amountPlanned"])
-        Monetary.getDefaultRounding();
-        Monetary.getDefaultRounding().apply(MoneyImpl.ofCents(123, "EUR"));
-        Monetary.getDefaultAmountType();
-        MonetaryFormats.getDefaultFormatProviderChain();
-        Monetary.getDefaultCurrencyProviderChain();
+        bind(CmsService.class).toProvider(FileBasedCmsServiceProvider.class).in(Singleton.class);
+        bind(TemplateEngine.class).toProvider(HandlebarsTemplateEngineProvider.class).in(Singleton.class);
+        bind(I18nResolver.class).toProvider(ConfigurableI18nResolverProvider.class).in(Singleton.class);
+        bind(HttpAuthentication.class).toProvider(BasicAuthenticationProvider.class).in(Singleton.class);
     }
 
     @Provides
-    public MultiControllerComponentResolver foo() {
+    public MultiControllerComponentResolver provideMultiControllerComponentResolver() {
         //here are also instanceof checks possible
         return new MultiControllerComponentResolverBuilder()
                 .add(CheckoutCommonComponent.class, controller -> controller.getFrameworkTags().contains("checkout"))
--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ version := "0.1.0-SNAPSHOT"
 
 scalaVersion := "2.11.8"
 
-val sunriseFrameworkVersion = "0.9.0"
+val sunriseFrameworkVersion = "0.11.0-2408161357-SNAPSHOT"
 
 lazy val root = (project in file("."))
   .enablePlugins(PlayJava)
@@ -27,7 +27,7 @@ libraryDependencies ++= Seq(
   "com.commercetools.sunrise" %% "shopping-cart" % sunriseFrameworkVersion,
   "com.commercetools.sunrise" %% "my-account" % sunriseFrameworkVersion,
   "com.commercetools.sunrise" %% "sbt-tasks" % sunriseFrameworkVersion,
-  "com.commercetools.sunrise" % "commercetools-sunrise-theme" % "0.60.0",
+  "com.commercetools.sunrise" % "commercetools-sunrise-theme" % "0.61.1",
   "org.webjars" %% "webjars-play" % "2.5.0-2"
 )
 
